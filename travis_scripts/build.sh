f4ff67b5e36c32097c7abecb9131f9093824fdc4
Jack Cable <jackhcable@gmail.com>
2020-06-17 15:04:14 -0500

    Ignore ssl verification for htmlproofer

--- travis_scripts/build.sh
+++ travis_scripts/build.sh
@@ -3,4 +3,4 @@
 npm install
 bundle exec jekyll build
 # We want to ignore things like <a href="#">Return to top</a>
-bundle exec htmlproofer ./_site --url-ignore "/#/"
+bundle exec htmlproofer ./_site --url-ignore "/#/" --typhoeus-config '{ "ssl_verifypeer": false }'
