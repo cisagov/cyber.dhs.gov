bdc811bf87a79a824653094eb9ae96417812b0cc
Cameron Dixon <cameron.dixon@cisa.dhs.gov>
2022-03-30 09:53:06 -0400

    remove travis

--- travis_scripts/build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-npm install
-bundle exec jekyll build
-# We want to ignore things like <a href="#">Return to top</a>
-bundle exec htmlproofer ./_site --url-ignore "/#/" --typhoeus-config '{ "ssl_verifypeer": false }'
